55af6cfd2499278f7f85c0e2434faceb337f947e
TotalpetcareJuo <petfoodjuo@gmail.com>
2026-02-26 16:58:48 +0900

    fix1111

--- src/app/reviews/page.tsx
+++ src/app/reviews/page.tsx
@@ -11,8 +11,7 @@ const fadeInUp = {
     hidden: { opacity: 0, y: 40 },
     visible: {
         opacity: 1,
-        y: 0,
-        transition: { duration: 0.8, ease: [0.16, 1, 0.3, 1] }
+        transition: { duration: 0.8, ease: [0.16, 1, 0.3, 1] as any }
     }
 };
 
